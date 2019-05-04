cc98dd7050b860d68236f6f5eb6cf7092f8643b8
Anton Vasiliev <aionbase@gmail.com>
2019-05-04 13:39:25 +0100

    Added AuthController skeleton

---
+++ src/controllers/AuthController.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace Phlexus\Modules\Admin\Controllers;
+
+use Phalcon\Mvc\Controller;
+
+final class AuthController extends Controller
+{
+    public function loginAction(): void
+    {
+
+    }
+
+    public function logoutAction(): void
+    {
+
+    }
+}
